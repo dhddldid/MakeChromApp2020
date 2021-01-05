a6e556271213c478a1649f8c55416fd20ef7cc31
dhddldid <dhddldid02@gmail.com>
2021-01-06 00:54:12 +0900

    Revert " If, else, and, or"
    This reverts commit 49f75ad8ed2f292120fc70b8fbb9faafdc40005f.

--- pratice/index.js
+++ pratice/index.js
@@ -1,10 +1,16 @@
-const age = prompt("How old are you")
-
-console.log(age)
-
-if( age > 18) {
-    console.log("you can drink");
+function handleResize(evnet){
+    console.log(event)
+    console.log("I have been resized")
 }
-else{
-    console.log("you cant");
+
+// handleResize() 라고 적을경우 함수를 바로 호출하는 것.
+// handleResize 라고했을 경우 resize 될 때마다 호출이 됨
+window.addEventListener("resize", handleResize);
+
+const title  = document.querySelector("#title");
+
+function handleClick(){
+    title.style.color = "blue";
 }
+
+title.addEventListener("click", handleClick);
