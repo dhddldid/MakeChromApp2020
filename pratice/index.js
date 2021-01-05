49f75ad8ed2f292120fc70b8fbb9faafdc40005f
dhddldid <dhddldid02@gmail.com>
2021-01-06 00:54:04 +0900

    If, else, and, or

--- pratice/index.js
+++ pratice/index.js
@@ -1,16 +1,10 @@
-function handleResize(evnet){
-    console.log(event)
-    console.log("I have been resized")
+const age = prompt("How old are you")
+
+console.log(age)
+
+if( age > 18) {
+    console.log("you can drink");
 }
-
-// handleResize() 라고 적을경우 함수를 바로 호출하는 것.
-// handleResize 라고했을 경우 resize 될 때마다 호출이 됨
-window.addEventListener("resize", handleResize);
-
-const title  = document.querySelector("#title");
-
-function handleClick(){
-    title.style.color = "blue";
+else{
+    console.log("you cant");
 }
-
-title.addEventListener("click", handleClick);
