d9bad83a085681055a3bcd55cee5b04fb5e29737
dhddldid <dhddldid02@gmail.com>
2021-01-05 23:32:02 +0900

    More Function Fun

--- Theory/index.js
+++ Theory/index.js
@@ -1,1 +1,35 @@
-console.log('Im Working. Im JS. Im Beautiful. Im worth it')
+//console.log('Im Working. Im JS. Im Beautiful. Im worth it')
+// First JS Variable
+//let a = 221;
+//let b = a - 5;
+//a = 4;
+//console.log(b, a);
+
+// constant, 상수 안정적이라는 뜻
+
+//var a = 22;
+//var b = a - 5;
+//a = 4;
+//console.log(b, a);
+
+
+// Object
+const dhddlInfo = {
+    name: "dldid",
+    age: 15000,
+    gender: "Male",
+    isGlasses: true,
+    favMovies: [
+        "The Lord of the Rings", "Kill Bill", "주유소 습격 사건", "Titanic", "신라의 달밤"
+    ],
+    favFood: [
+        {
+            name: "pork cutlet"
+        },
+        {
+            name: "NoBrand burger",
+            fatty: "Double"
+        }
+    ]
+}
+console.log(dhddlInfo.favFood[0])
---
+++ pratice/index.js
@@ -0,0 +1,18 @@
+//function sayHello(potato, chicken){
+//    //console.log('Hello!', potato, " you have", chicken);
+//    return console.log(`Hello ${potato} you are ${chicken} years old`);
+//}
+
+//const greetdhddldid = sayHello("dhddldid", 14)
+
+
+//console.log(greetdhddldid)
+
+const caculator = {
+    plus: function(a, b){
+        return a+b;
+    }
+}
+
+const plus = caculator.plus(5,5);
+console.log(plus)
